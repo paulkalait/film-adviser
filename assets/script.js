5e5be5590ad97f9bf7908640b5dda5e56164651b
Amber Bauer <amberbauer@Ambers-MBP.hsd1.ct.comcast.net>
2022-03-09 20:55:28 -0500

    read review button updated

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,17 @@ var movieInput = document.querySelector("#input-field");
 var movieFormEl = document.querySelector("#movieform");
 var submitbtnEl = document.querySelector("#search-btn");
 
+// Get the modal
+//var modal = document.querySelector("#myModal");
+
+// Get the button that opens the modal
+//var btn = document.querySelector("#myBtn");
+
+// Get the <span> element that closes the modal
+//var span = document.querySelector(".close")[0];
+
+
+
 var getMovieApi = function (event) {
   event.preventDefault();
   var requestOptions = {
@@ -43,9 +54,16 @@ var getMovieApi = function (event) {
                  <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">User: ${reviewData.items[i].username}</h5>
                 <h6 class="text-gray-900 text-xl leading-tight font-medium mb-2">Review: ${reviewData.items[i].title} </h6>
                 <img class="images" src="${moviedata.results[i].image}"/>
-                <p class="text-gray-700 text-base mb-4 reviewText">${reviewData.items[i].content}</p>
+                <button id="myBtn">Open Modal</button>
+                      <div id="myModal" class="modal">
+                      <div class="modal-content">
+                      <span class="close">&times;</span>
+                      <p class="text-gray-700 text-base mb-4 reviewText">${reviewData.items[i].content}</p>
+                  </div>
+                    </div>
                  </div>
-                </div>`
+                    </div>
+                    ` 
                 console.log(reviewData);
               }
             }
@@ -58,5 +76,20 @@ var getMovieApi = function (event) {
 
 
 // getMovieApi();
+// When the user clicks on the button, open the modal
+//btn.onclick = function() {
+//  modal.style.display = "block";
+// }
 
+// When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
 submitbtnEl.addEventListener("click", getMovieApi);
